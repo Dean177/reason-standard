8d060700055611bfeed0a7ad6a4b9b4a85c9c99f
Dean Merchant <deanmerchant@gmail.com>
2020-03-03 21:39:01 +0000

    Re enable syntax highlighting even though its super slow

--- website/src/components/CodeBlock.js
+++ website/src/components/CodeBlock.js
@@ -2,77 +2,13 @@ import * as React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import prismLightTheme from 'prism-react-renderer/themes/nightOwlLight';
 import prismDarkTheme from 'prism-react-renderer/themes/nightOwl';
-import refmt from 'reason';
 import { useSyntax } from './Syntax';
 import { useTheme, colors } from '../theme';
-
-// export const CodeBlock = ({ code, ...props }) => {
-//   let [syntax, _] = useSyntax();
-//   let [theme] = useTheme();
-//   let content = React.useMemo(() => {
-//     let lines = code.split('\n');
-//     let firstNonEmptyLine = 0;
-//     while (lines[firstNonEmptyLine].trim() === '') {
-//       firstNonEmptyLine++;
-//     }
-//     let spacesToFirstCharacter = 0;
-//     while (lines[firstNonEmptyLine][spacesToFirstCharacter] == ' ') {
-//       spacesToFirstCharacter++;
-//     }
-//     let codeContent = lines
-//       .slice(firstNonEmptyLine, lines.length)
-//       .map(line => line.slice(spacesToFirstCharacter, line.length))
-//       .join('\n')
-//       .toString();
-
-//     try {
-//       if (!props.language && syntax === 'reason') {
-//         codeContent = refmt.printRE(refmt.parseML(code));
-//       }
-//     } catch (error) {
-//       console.error(error.message, code);
-//     }
-//     return codeContent
-//   }, [code]);
-//   return (
-//     <Highlight
-//       {...defaultProps}
-//       code={content}
-//       theme={theme === 'light' ? prismLightTheme : prismDarkTheme}
-//       language={props.language || 'ocaml' || syntax}
-//       {...props}
-//     >
-//       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-//         <pre
-//           className={'CodeBlock ' + className}
-//           style={style}
-//           p={3}
-//           css={css`
-//             border: 1px solid ${colors.grey.base};
-//             border-radius: 3px;
-//             padding: 10px 12px;
-//           `}
-//         >
-//           {cleanTokens(tokens).map((line, i) => {
-//             const lineProps = getLineProps({ line, key: i });
-//             return (
-//               <div {...lineProps} key={line + i}>
-//                 {line.map((token, key) => {
-//                   return <span {...getTokenProps({ token, key })} />;
-//                 })}
-//               </div>
-//             );
-//           })}
-//         </pre>
-//       )}
-//     </Highlight>
-//   );
-// };
 
 export const CodeBlock = ({ code, ...props }) => {
   let [syntax, _] = useSyntax();
   let [theme] = useTheme();
-  let language = typeof code === 'object' ? syntax : (props.language || 'ocaml');
+  let language = typeof code === 'object' ? syntax : props.language || 'ocaml';
   let lines = (typeof code === 'object' ? code[syntax] : code).split('\n');
   let firstNonEmptyLine = 0;
   while (lines[firstNonEmptyLine].trim() === '') {
@@ -85,20 +21,54 @@ export const CodeBlock = ({ code, ...props }) => {
   let content = lines
     .slice(firstNonEmptyLine, lines.length)
     .map(line => line.slice(spacesToFirstCharacter, line.length))
+    .filter(line => line.trim().length > 0)
     .join('\n')
     .toString();
 
   return (
-    <pre
-      className={'CodeBlock'}
-      css={css`
-        border: 1px solid ${colors.grey.base};
-        border-radius: 3px;
-        padding: 10px 12px;
-        background-color: ${theme};
-      `}
+    <Highlight
+      {...defaultProps}
+      code={content}
+      theme={theme === 'light' ? prismLightTheme : prismDarkTheme}
+      language={props.language || 'ocaml' || syntax}
+      {...props}
     >
-      {content}
-    </pre>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          className={'CodeBlock ' + className}
+          style={style}
+          p={3}
+          css={css`
+            border: 1px solid ${colors.grey.base};
+            border-radius: 3px;
+            padding: 10px 12px;
+          `}
+        >
+          {tokens.map((line, i) => {
+            return (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => {
+                  return <span {...getTokenProps({ token, key })} />;
+                })}
+              </div>
+            );
+          })}
+        </pre>
+      )}
+    </Highlight>
   );
+
+  // return (
+  //   <pre
+  //     className={'CodeBlock'}
+  //     css={css`
+  //       border: 1px solid ${colors.grey.base};
+  //       border-radius: 3px;
+  //       padding: 10px 12px;
+  //       background-color: ${theme};
+  //     `}
+  //   >
+  //     {content}
+  //   </pre>
+  // );
 };
